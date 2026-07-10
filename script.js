58fcbc9be09d5f4a5c8ebded16fe6a0905b44f69
Jac-bot-max <brmany23@gmail.com>
2026-07-10 23:42:12 +0200

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-F
+// Splash Screen
+window.onload = () => {
+    setTimeout(() => {
+        const splash = document.getElementById("splash");
+        if (splash) splash.style.display = "none";
+    }, 4500);
+};
+
+// Botão Entrar
+const botao = document.querySelector("button");
+
+if (botao) {
+    botao.addEventListener("click", () => {
+        alert("Bem-vindo ao Word Music Lite! 🚀");
+        // Depois vamos trocar isso para abrir o Feed.
+        // Exemplo:
+        // window.location.href = "feed.html";
+    });
+}
